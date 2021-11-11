b7746fbe5dfc39d66c926b85b8a32f451ad18a3a
hamza <hamzabakali19@gmail.com>
2021-11-12 00:58:58 +0100

    Refactor services

--- myPetClinicData/src/main/java/com/example/mypetclinic/Services/VetService.java
+++ myPetClinicData/src/main/java/com/example/mypetclinic/Services/VetService.java
@@ -1,15 +1,8 @@
 package com.example.mypetclinic.Services;
 
-import com.example.mypetclinic.model.Pet;
 import com.example.mypetclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
+public interface VetService  extends CrudService<Vet,Long> {
 
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
